271831ffc78afe83f1de2ac86585f25e02bb33bc
Arjun Puri <arjun_261094@hotmail.com>
2025-03-11 17:26:13 +0100

    add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import { Button } from '@/components/buttons';
+import { Section } from '@/components/section';
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-sans">
-      Portfolio under construction
-    </div>
+    <Section className="font-sans min-h-[100svh] flex flex-col items-center justify-center gap-16">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-4xl text-center font-sans font-black">Hi! I&apos;m Arjun</h1>
+        <p className="text-center text-2xl text-lime">Welcome to my portfolio</p>
+      </div>
+
+      <div className="flex gap-4">
+        <Button isLinkButton href="/cv">
+          View CV
+        </Button>
+        <Button isLinkButton href="/projects" variant="secondary">
+          Projects
+        </Button>
+      </div>
+    </Section>
   );
 }
