38f849665c63c984fd0f23b149ad072c5d66be57
Arjun Puri <arjun_261094@hotmail.com>
2025-03-12 21:02:08 +0100

    Added new variant for print

--- src/components/badge/index.tsx
+++ src/components/badge/index.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx';
 
-type BadgeColor = 'blue' | 'lime' | 'violet' | 'white';
+type BadgeColor = 'blue' | 'blue-700' | 'lime' | 'violet' | 'white';
 
 type BadgeProps = {
   children: React.ReactNode;
@@ -15,6 +15,7 @@ export const Badge = ({ children, className, hasHover = false, onClick, color = 
     'inline-block px-3 py-1 font-sans text-sm rounded-full',
     {
       'bg-blue/10 text-blue border border-blue/20 hover:bg-blue/20': color === 'blue',
+      'bg-blue-700/10 text-blue-700 border border-blue-700/40': color === 'blue-700',
       'bg-lime/10 text-lime border border-lime/20 hover:bg-lime/20': color === 'lime',
       'bg-violet/10 text-violet border border-violet/20 hover:bg-violet/20': color === 'violet',
       'bg-white/10 text-white border border-white/20 hover:bg-white/20': color === 'white',
