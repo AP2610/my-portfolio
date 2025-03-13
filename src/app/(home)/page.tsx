d096cdac7e79e7f74c58a7970d0d26efe82b0157
Arjun Puri <arjun_261094@hotmail.com>
2025-03-13 17:35:29 +0100

    Animate hero text

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,5 @@
 import { About } from '@/components/about';
+import { AnimatedElementPresence } from '@/components/animated-element-presence';
 import { Button } from '@/components/buttons';
 import { Section } from '@/components/section';
 import { FaArrowDown } from 'react-icons/fa';
@@ -9,25 +10,25 @@ const Home = () => {
       <Section
         className="custom-background-violet flex min-h-[100svh] flex-col items-center justify-center"
         isFullWidth
-        // The id is needed for my animated styling on the hero to work. Note to self: Consider getting rid of it and the associated logic.
+        // The id is needed for my animated styling on the header to work. Note to self: Consider getting rid of it and the associated logic.
         id="hero"
       >
-        <Section className="flex h-full flex-col items-center justify-center gap-16 font-sans">
-          <div className="flex flex-col gap-4">
-            <h1 className="text-center font-sans text-4xl font-black">Hi! I&apos;m Arjun</h1>
-            <p className="text-center text-2xl font-light text-lime">Welcome to my portfolio</p>
-          </div>
-
-          <div className="flex gap-4">
-            <Button isLinkButton href="/cv">
-              View CV
-            </Button>
-
-            <Button variant="secondary" scrollTo="about">
-              About Me <FaArrowDown className="bounce-vertical ml-2" />
-            </Button>
-          </div>
-        </Section>
+        <AnimatedElementPresence entryAnimationDelay={0.2} animationProperty="opacity">
+          <Section className="flex h-full flex-col items-center justify-center gap-16 font-sans">
+            <div className="flex flex-col gap-4">
+              <h1 className="text-center font-sans text-4xl font-black">Hi! I&apos;m Arjun</h1>
+              <p className="text-center text-2xl font-light text-lime">Welcome to my portfolio</p>
+            </div>
+            <div className="flex gap-4">
+              <Button isLinkButton href="/cv">
+                View CV
+              </Button>
+              <Button variant="secondary" scrollTo="about">
+                About Me <FaArrowDown className="bounce-vertical ml-2" />
+              </Button>
+            </div>
+          </Section>
+        </AnimatedElementPresence>
       </Section>
 
       <Section id="about" isFullWidth hasTopPadding hasBottomPadding>
