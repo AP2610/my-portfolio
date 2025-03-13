32bc966b0fed767a54dfe086918860f7bff6e0f9
Arjun Puri <arjun_261094@hotmail.com>
2025-03-13 23:07:23 +0100

    Adjustments to about section

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -3,7 +3,6 @@ import { Section } from '@/components/section';
 import { Badge } from '@/components/badge';
 import { InfoBox } from '@/components/info-box';
 import { Blockquote } from '@/components/blockquote';
-import { TextHighlight } from '@/components/text-highlight';
 
 const specialisations = ['JavaScript', 'TypeScript', 'React', 'Next.js', 'Tailwind CSS', 'Sanity CMS'];
 
@@ -47,9 +46,9 @@ export const About = () => {
 
       <p>
         With a service mindset shaped by a hospitality education, I bring strong interpersonal skills and a keen eye for detail,
-        ensuring that both user experience and technical execution are of the highest quality. Having lived in{' '}
-        <TextHighlight color="blue">Australia, India, Kenya, Switzerland, and now the Netherlands</TextHighlight>, I excel at
-        navigating cultural dynamics and seamlessly integrating into diverse teams.
+        ensuring that both user experience and technical execution are of the highest quality. Having lived in Australia, India,
+        Kenya, Switzerland, and now the Netherlands, I excel at navigating cultural dynamics and seamlessly integrating into
+        diverse teams.
       </p>
 
       <p>
@@ -61,31 +60,30 @@ export const About = () => {
       <h3 className="text-xl">AI in My Workflow</h3>
 
       <p>
-        In the past year, I have worked extensively with AI tools like GitHub Copilot, Augment, and ChatGPT and found way to
-        integrate them into my development workflow, leveraging them to optimise development processes, automate repetitive tasks,
-        and enhance productivity. Beyond work, I use AI extensively in my personal life, whether it&apos;s for DIY projects or
-        recipe development—helping me refine and improve ideas efficiently.
+        In the past year, I have worked with AI tools like GitHub Copilot, Augment, and ChatGPT and found ways to integrate them
+        into my development workflow, leveraging them to optimise development processes, automate repetitive tasks, and enhance
+        productivity. Beyond work, I use AI extensively in my personal life, whether it&apos;s for DIY projects or recipe
+        development—helping me refine and improve ideas efficiently.
       </p>
 
       <h3 className="text-xl">Beauty in Simplicity</h3>
 
       <p>
-        While some pride themselves on complex solutions, I value clarity and simplicity in code. I often find myself breaking
-        down complex structures into more readable, maintainable, and efficient solutions. A well-structured, simple codebase
-        improves both productivity and collaboration. Having said that, I myself have written some overly complex logic in the
-        past, only to find myself returning months later trying to demystify it. That&apos;s only reinforced my belief in KISS -
-        Keep It Simple, Stupid. Additionally, working in teams with a varying range of experience, I&apos;ve learnt the importance
-        of keeping code understandable by all levels. I am far from perfect at this, but I always try to keep it in mind when
-        writing code.
+        While some pride themselves on complex solutions, I value clarity and simplicity in code. A well-structured, simple
+        codebase improves both productivity and collaboration. Having said that, I myself have written some overly complex logic
+        in the past, only to find myself returning months later trying to demystify it. That&apos;s only reinforced my belief in
+        KISS - Keep It Simple, Stupid. Additionally, working in teams with a varying range of experience, I&apos;ve learnt the
+        importance of keeping code understandable by all levels. I am far from perfect at this, but I always try to keep it in
+        mind when writing code.
       </p>
 
       <h3 className="text-xl">Experience with Large Codebases & Clean Code Philosophy</h3>
 
       <p>
-        I have worked extensively with large-scale codebases, particularly at the Corona Dashboard and Blokker. I am a strong
-        advocate for refactoring—a clean, well-structured codebase is paramount to long-term productivity and maintainability.
-        However, I also understand the balance between code quality and efficient delivery. Like my mother taught me, &quot;a
-        clean closet makes for a clear mind.&quot;
+        I have experience with large-scale codebases, particularly at the Corona Dashboard and Blokker. I am a strong advocate for
+        refactoring—a clean, well-structured codebase is paramount to long-term productivity and maintainability. However, I also
+        understand the need for balance between code quality and efficient delivery. Like my mother taught me, &quot;a clean
+        closet makes for a clear mind.&quot;
       </p>
 
       <p>
