85f3869a9b6037dca6bcd74820014a049afff00f
Arjun Puri <arjun_261094@hotmail.com>
2025-03-13 16:02:41 +0100

    Update violet badge

--- src/components/badge/index.tsx
+++ src/components/badge/index.tsx
@@ -19,7 +19,7 @@ export const Badge = ({ children, className, hasLink = false, onClick, color = '
       'bg-blue/10 text-blue border border-blue/20': color === 'blue',
       'bg-blue-700/10 text-blue-700 border border-blue-700/40': color === 'blue-700',
       'bg-lime/10 text-lime border border-lime/20': color === 'lime',
-      'bg-violet/10 text-violet border border-violet/20': color === 'violet',
+      'bg-violet/15 text-violet-400 border border-violet/40': color === 'violet',
       'bg-white/10 text-white border border-white/20': color === 'white',
 
       // Hover styles only applied when hasLink is true
