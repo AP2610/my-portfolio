c1e77eb3598586fbb7a7b515a7a1d3a93ebb6f91
Arjun Puri <arjun_261094@hotmail.com>
2025-03-15 18:32:42 +0100

    add additional favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,14 @@ const raleway = Raleway({
 export const metadata: Metadata = {
   title: "Arjun Puri's Portfolio",
   description: 'A portfolio website for Arjun Puri representing his work and skills.',
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [{ url: '/apple-touch-icon.png' }],
+  },
 };
 
 const RootLayout = ({
