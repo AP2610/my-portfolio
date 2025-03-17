9178b760d881ae5adf64f3d35af2abc90ae6c774
Arjun Puri <arjun_261094@hotmail.com>
2025-03-17 21:00:47 +0100

    Theme switcher WIP

--- src/components/theme-switcher/index.tsx
+++ src/components/theme-switcher/index.tsx
@@ -4,12 +4,14 @@ import { ThemeContext } from '@/context/theme-context';
 import { useContext } from 'react';
 import { FaMoon, FaSun } from 'react-icons/fa';
 import { Button } from '../buttons';
+import clsx from 'clsx';
 
-export const ThemeSwitcher = () => {
+export const ThemeSwitcher = ({ className }: { className?: string }) => {
   const { theme, handleThemeToggle } = useContext(ThemeContext);
+  const classes = clsx('text-accent-violet hover:text-violet-700 dark:text-accent-violet dark:hover:text-violet-600', className);
 
   return (
-    <Button variant="icon-button" onClick={handleThemeToggle}>
+    <Button variant="icon-button" onClick={handleThemeToggle} className={classes}>
       {theme === 'dark' ? <FaSun /> : <FaMoon />}
     </Button>
   );
