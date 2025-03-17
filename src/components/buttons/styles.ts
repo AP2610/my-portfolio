40b4c5b71d52173cffd3db5d6a4288a40453c876
Arjun Puri <arjun_261094@hotmail.com>
2025-03-17 20:14:42 +0100

    Update button component with new variant

--- src/components/buttons/styles.ts
+++ src/components/buttons/styles.ts
@@ -2,6 +2,8 @@ export const buttonStyles = {
   variants: {
     'icon-button': 'w-[fit-content] p-4',
     primary: 'bg-primary text-primary-foreground hover:bg-primary/80',
+    'primary-outline':
+      'bg-white-600/20 dark:bg-primary/20 text-foreground border-2 border-white-600 dark:border-primary hover:bg-white-600/40 dark:hover:bg-primary/40',
     secondary: 'bg-accent-lime/20 text-accent-lime-foreground border-2 border-accent-lime hover:bg-accent-lime/5',
     'blue-secondary': 'text-accent-blue border-2 border-accent-blue hover:bg-accent-blue/10',
   },
