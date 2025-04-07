b3235d44991507dfa896852ac748e26f84a2e342
Arjun Puri <arjun_261094@hotmail.com>
2025-04-07 19:10:30 +0200

    Add button to about me card

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,6 +4,7 @@ import { IconList } from '@/components/features/home/icon-list';
 import { Section } from '@/components/layout/section';
 import { AnimatedElementPresence } from '@/components/ui/animated-element-presence';
 import { AnimatedRadialGradientBackground } from '@/components/ui/animated-radial-gradient-background';
+import { Button } from '@/components/ui/buttons';
 import { MyLink } from '@/components/ui/my-link';
 import { SocialIconLink } from '@/components/ui/social-icon-link';
 
@@ -13,16 +14,16 @@ import ReactIcon from '@/public/icons/react-40.png';
 import SanityIcon from '@/public/icons/sanity-180.png';
 import TailwindIcon from '@/public/icons/tailwindcss-48.png';
 import TypeScriptIcon from '@/public/icons/typescript-50.png';
+import { FaArrowDown } from 'react-icons/fa';
 
 const Home = () => {
   return (
     <>
       <Section
-        className="relative flex flex-col items-center justify-center"
+        className="relative flex min-h-screen flex-col items-center justify-center"
         isFullWidth
-        hasTopPadding
         hasBottomPadding
-        hasHeaderMarginTop
+        hasHeaderPaddingTop
         // TODO: Get rid of the header logic on the hero
         // The id is needed for my animated styling on the header to work. Note to self: Consider getting rid of it and the associated logic.
         id="hero"
@@ -36,24 +37,29 @@ const Home = () => {
             className="grid grid-cols-4 grid-rows-3 gap-6"
           >
             <HomepageCard>
+              {/* TODO: make a heading component that takes level, size, weight, and color props */}
               <h1 className="mb-4 text-3xl font-semibold">Hi, I'm Arjun</h1>
-              <p className="text-gray-600">Welcome to my portfolio!</p>
+              <p className="text-gray-500">Welcome to my portfolio!</p>
             </HomepageCard>
 
             <HomepageCard>
               <h2 className="mb-4 text-3xl font-semibold">5 years</h2>
-              <p className="text-gray-600">Of frontend development experience</p>
+              <p className="text-gray-500">Of frontend development experience</p>
             </HomepageCard>
 
-            <HomepageCard className="col-span-2 row-span-2" textAlign="left" alignItems="start">
-              <h2 className="mb-4 text-3xl font-semibold">About me</h2>
+            <HomepageCard className="col-span-2 row-span-2 space-y-4" textAlign="left" alignItems="start">
+              <h2 className="text-3xl font-semibold">About me</h2>
 
-              <p className="text-gray-600">
+              <p className="text-gray-500">
                 I'm a frontend engineer with a passion for creating beautiful, interactive, and accessible applications that
                 prioritise the user experience. While trained as a full-stack developer, I gravitated towards the frontend for its
                 direct impact on users. I thrive on transforming ideas into functional, visually appealing web applications and
                 believe in clear, honest communication—no beating around the bush, just constructive and transparent discussions.
               </p>
+
+              <Button variant="secondary" scrollTo="about" className="ml-auto">
+                Read More <FaArrowDown className="bounce-vertical ml-2" />
+              </Button>
             </HomepageCard>
 
             <HomepageCard className="col-span-2" textAlign="left" alignItems="start">
@@ -63,7 +69,7 @@ const Home = () => {
                 <IconList iconList={[NextJsIcon, ReactIcon, JavaScriptIcon, TypeScriptIcon, TailwindIcon, SanityIcon]} />
               </div>
 
-              <p className="text-gray-600">Next.js, React, JavaScript, TypeScript, Tailwind CSS, Sanity CMS</p>
+              <p className="text-gray-500">Next.js, React, JavaScript, TypeScript, Tailwind CSS, Sanity CMS</p>
             </HomepageCard>
 
             <HomepageCard>
