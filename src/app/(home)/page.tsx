cd597bb4f46c89f5b8ecff0cbb2ee25026fd3080
Arjun Puri <arjun_261094@hotmail.com>
2025-04-08 20:54:23 +0200

    Remove animation, move to layout

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,28 +1,20 @@
 import { About } from '@/components/features/about';
 import { HomepageCardGrid } from '@/components/features/home/home-page-card-grid';
 import { Section } from '@/components/layout/section';
-import { AnimatedElementPresence } from '@/components/ui/animated-element-presence';
 import { AnimatedRadialGradientBackground } from '@/components/ui/animated-radial-gradient-background';
-import { AnimatedText } from '@/components/ui/animated-text';
 
 const Home = () => {
   return (
     <>
-      <Section className="relative flex min-h-[100dvh] flex-col justify-around gap-16 pb-16 pt-16 md:gap-8 md:pt-8">
+      <Section className="relative flex min-h-dvh flex-col justify-around gap-16 py-8 md:gap-8">
         <AnimatedRadialGradientBackground />
 
-        <h1 className="hidden text-4xl text-foreground md:block">
-          <AnimatedText text="Frontend Developer" />
-        </h1>
-
         <div className="space-y-4 text-foreground md:hidden">
-          <p className="text-3xl font-black">Hi, I'm Arjun</p>
-          <p>I'm a frontend developer based in the Netherlands.</p>
+          <h1 className="text-3xl">Hi, I'm Arjun</h1>
+          <p className="text-lg">I'm a frontend engineer based in the Netherlands.</p>
         </div>
 
-        <AnimatedElementPresence entryAnimationDelay={0.2} animationProperty="opacity">
-          <HomepageCardGrid />
-        </AnimatedElementPresence>
+        <HomepageCardGrid />
       </Section>
 
       <Section id="about" isFullWidth hasTopPadding hasBottomPadding>
