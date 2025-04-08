1e808ccc6249e355a071326c0959315f0d2cc800
Arjun Puri <arjun_261094@hotmail.com>
2025-04-09 01:16:33 +0200

    Create component for small screen title and description, introduce hack to avoid layout shift

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,23 +1,25 @@
 import { About } from '@/components/features/about';
 import { HomepageCardGrid } from '@/components/features/home/home-page-card-grid';
+import { SmallScreenHeroTitle } from '@/components/features/home/small-screen-hero-title';
 import { Section } from '@/components/layout/section';
 import { AnimatedRadialGradientBackground } from '@/components/ui/animated-radial-gradient-background';
 
 const Home = () => {
   return (
     <>
-      <Section className="relative flex min-h-dvh flex-col justify-around gap-16 py-8 md:gap-8">
+      <Section
+        className="relative flex min-h-dvh flex-col justify-around gap-16 md:gap-0"
+        paddingTop="small"
+        paddingBottom="small"
+      >
         <AnimatedRadialGradientBackground />
 
-        <div className="space-y-4 text-foreground md:hidden">
-          <h1 className="text-3xl">Hi, I'm Arjun</h1>
-          <p className="text-lg">I'm a frontend engineer based in the Netherlands.</p>
-        </div>
+        <SmallScreenHeroTitle className="md:hidden" />
 
         <HomepageCardGrid />
       </Section>
 
-      <Section id="about" isFullWidth hasTopPadding hasBottomPadding>
+      <Section id="about" paddingTop="medium" paddingBottom="medium">
         <About />
       </Section>
     </>
