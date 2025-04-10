ac40941bb926140ccfa4f541dadde54673fd285b
Arjun Puri <arjun_261094@hotmail.com>
2025-04-10 19:05:13 +0200

    Refactor to accept additional media queries

--- src/hooks/use-media-query.ts
+++ src/hooks/use-media-query.ts
@@ -5,39 +5,44 @@ import { useEffect, useState } from 'react';
 export type Breakpoints = 'sm' | 'md' | 'lg' | 'xl' | '2xl';
 
 const twBreakpoints: Record<Breakpoints, string> = {
-  sm: '640px',
-  md: '768px',
-  lg: '1024px',
-  xl: '1280px',
-  '2xl': '1536px',
+  sm: '(min-width: 640px)',
+  md: '(min-width: 768px)',
+  lg: '(min-width: 1024px)',
+  xl: '(min-width: 1280px)',
+  '2xl': '(min-width: 1536px)',
 } as const;
-
-// TODO: can be updated to accept additional media queries like color scheme, reduced motion, and orientation.
 
 /**
  * // Returns true when viewport width >= 768px (md)
  * const isMediumScreenOrLarger = useMediaQuery('md');
  */
-export const useMediaQuery = (breakpoint: 'sm' | 'md' | 'lg' | 'xl' | '2xl') => {
+export const useMediaQuery = (mediaQuery: 'sm' | 'md' | 'lg' | 'xl' | '2xl' | string) => {
   const [matches, setMatches] = useState<boolean>(false);
 
+  let query;
+  if (mediaQuery in twBreakpoints) {
+    query = twBreakpoints[mediaQuery as Breakpoints];
+  } else {
+    query = mediaQuery;
+  }
+
   useEffect(() => {
-    const mediaQuery = window.matchMedia(`(min-width: ${twBreakpoints[breakpoint]})`);
+    const media = window.matchMedia(query);
 
     // Set the matches state initially
-    setMatches(mediaQuery.matches);
+    setMatches(media.matches);
 
     // Update the matches state when the media query changes when crossing a specific breakpoint
-    const handleMediaQueryChange = () => {
-      setMatches(mediaQuery.matches);
+    const handleMediaChange = () => {
+      setMatches(media.matches);
     };
 
-    mediaQuery.addEventListener('change', handleMediaQueryChange);
+    media.addEventListener('change', handleMediaChange);
 
     return () => {
-      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+      media.removeEventListener('change', handleMediaChange);
     };
-  }, [breakpoint]);
+  }, [query]);
 
   return matches;
 };
