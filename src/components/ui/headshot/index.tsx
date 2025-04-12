2017e084ba764773c58fb95cef058bb2c3335912
Arjun Puri <arjun_261094@hotmail.com>
2025-04-12 20:55:18 +0200

    Add new size

--- src/components/ui/headshot/index.tsx
+++ src/components/ui/headshot/index.tsx
@@ -3,14 +3,15 @@ import HeadshotImage from '@/public/cv-images/headshot.webp';
 import clsx from 'clsx';
 
 type HeadshotProps = {
-  size: 'small' | 'medium' | 'large';
+  size: 'xs' | 'small' | 'medium' | 'large';
   className?: string;
 };
 
 export const Headshot = ({ size, className }: HeadshotProps) => {
   const containerClasses = clsx(
-    'rounded-full overflow-hidden flex-shrink-0 border-2 border-accent-lime hover:scale-105 transition-transform duration-300',
+    'rounded-full overflow-hidden flex-shrink-0 border-2 border-accent-lime hover:scale-105 transition-all duration-300',
     {
+      'w-8 h-8': size === 'xs',
       'w-32 h-32': size === 'small',
       'w-48 h-48': size === 'medium',
       'w-64 h-64': size === 'large',
