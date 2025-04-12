b3c39980c32794571acd152cc4078c3464e24430
Arjun Puri <arjun_261094@hotmail.com>
2025-04-12 20:57:09 +0200

    adjust component to handle collapsed state by adding nav icons

--- src/components/layout/navigation/desktop-navigation.tsx
+++ src/components/layout/navigation/desktop-navigation.tsx
@@ -1,21 +1,46 @@
+import clsx from 'clsx';
+import { HiOutlineCodeBracketSquare } from 'react-icons/hi2';
+import { TbFileCv, TbHome } from 'react-icons/tb';
 import { NavLink } from './nav-link';
 import { NAVIGATION_LINKS } from './nav-links';
-import clsx from 'clsx';
 
 type DesktopNavigationProps = {
+  isCollapsed: boolean;
   className?: string;
 };
 
-export const DesktopNavigation = ({ className }: DesktopNavigationProps) => {
+const iconProps = {
+  strokeWidth: 1.2,
+  className: 'h-6 w-6 mx-auto',
+};
+
+const COLLAPSED_NAV_LINKS = [
+  {
+    href: '/',
+    label: <TbHome {...iconProps} />,
+  },
+  {
+    href: '/cv',
+    label: <TbFileCv {...iconProps} />,
+  },
+  {
+    href: '/projects',
+    label: <HiOutlineCodeBracketSquare {...iconProps} />,
+  },
+] as const;
+
+export const DesktopNavigation = ({ isCollapsed, className }: DesktopNavigationProps) => {
   const navclasses = clsx('w-full flex justify-center', className);
 
   return (
     <nav className={navclasses}>
       <ul className="flex w-full flex-col items-center gap-12" role="Navigation list">
-        {NAVIGATION_LINKS.map((link, index) => (
-          <NavLink key={`${index}-${link.href}`} href={link.href} fontSize="small">
-            {link.label}
-          </NavLink>
+        {(isCollapsed ? COLLAPSED_NAV_LINKS : NAVIGATION_LINKS).map((link) => (
+          <li key={link.href} className="flex w-full text-center">
+            <NavLink href={link.href} fontSize="small">
+              {link.label}
+            </NavLink>
+          </li>
         ))}
       </ul>
     </nav>
