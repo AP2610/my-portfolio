0fc2f5df4a11b46796f2bf3e46f2fddf3d348a82
Arjun Puri <arjun_261094@hotmail.com>
2025-04-13 20:11:17 +0200

    Adjust project card component so the whole thiing is clickable

--- src/components/features/projects/project-card/index.tsx
+++ src/components/features/projects/project-card/index.tsx
@@ -1,28 +1,33 @@
 'use client';
 
 import { ProjectType } from '@/app/projects/data';
-import { Button } from '@/components/ui/button';
 import { ModalDialog } from '@/components/ui/modal-dialog';
 import { useRef } from 'react';
 import { ProjectDetails } from '../project-details';
+import { buttonStyles } from '@/components/ui/button/styles';
 
 type ProjectCardProps = {
   project: ProjectType;
 };
+
+const inlineButtonStyle = buttonStyles.variants.inline;
 
 export const ProjectCard = ({ project }: ProjectCardProps) => {
   const dialogRef = useRef<HTMLDialogElement | null>(null);
 
   return (
     <>
-      <article className="flex h-full max-w-[350px] flex-col overflow-hidden rounded-lg bg-cosmic-latte-600/10 p-4 shadow-md transition-transform hover:scale-[1.02] hover:transform dark:border-2 dark:border-rich-black-400 dark:bg-rich-black-400/30">
-        <h2 className="text-2xl text-accent-electric-blue">{project.company}</h2>
-        <p className="text-xl font-semibold text-foreground">{project.role}</p>
+      <button
+        onClick={() => dialogRef.current?.showModal()}
+        className="flex flex-col gap-6 rounded-md bg-homepage-card-bg p-6 text-left text-foreground shadow-md transition-transform hover:scale-[1.02]"
+      >
+        <div className="space-y-2">
+          <h2 className="text-xl font-light">{project.company}</h2>
+          <p className="text-foreground text-gray-500">{project.role}</p>
+        </div>
 
-        <Button variant="primary-outline" className="mt-auto" onClick={() => dialogRef.current?.showModal()}>
-          Read More
-        </Button>
-      </article>
+        <span className={`${inlineButtonStyle} ml-auto mt-auto`}>Read More</span>
+      </button>
 
       <ModalDialog ref={dialogRef}>
         <ProjectDetails {...project} />
