efbb6f45ede90f80fc43e529dfaf7b37b1a4c682
Arjun Puri <arjun_261094@hotmail.com>
2025-04-17 16:35:54 +0200

    Create generic card

--- src/components/features/projects/project-card/index.tsx
+++ src/components/features/projects/project-card/index.tsx
@@ -5,6 +5,7 @@ import { ModalDialog } from '@/components/ui/modal-dialog';
 import { useRef } from 'react';
 import { ProjectDetails } from '../project-details';
 import { buttonStyles } from '@/components/ui/button/styles';
+import { Card } from '@/components/ui/card';
 
 type ProjectCardProps = {
   project: ProjectType;
@@ -17,17 +18,19 @@ export const ProjectCard = ({ project }: ProjectCardProps) => {
 
   return (
     <>
-      <button
-        onClick={() => dialogRef.current?.showModal()}
-        className="flex flex-col gap-6 rounded-md bg-homepage-card-bg p-6 text-left text-foreground shadow-md transition-all duration-300 hover:scale-[1.02] hover:shadow-lg"
-        aria-label="Read more about project"
-      >
-        <div className="space-y-2">
-          <h2 className="text-xl font-light">{project.company}</h2>
-          <p className="text-card-body-text-color">{project.role}</p>
-        </div>
+      <button onClick={() => dialogRef.current?.showModal()} aria-label={`Read more about the ${project.company} project`}>
+        <Card
+          textAlign="left"
+          alignItems="start"
+          className="h-full gap-6 transition-all duration-300 hover:scale-105 hover:shadow-lg"
+        >
+          <div className="space-y-2">
+            <h2 className="text-xl font-light">{project.company}</h2>
+            <p className="text-card-body-text-color">{project.role}</p>
+          </div>
 
-        <span className={`${inlineButtonStyle} ml-auto mt-auto`}>Read More</span>
+          <span className={`${inlineButtonStyle} ml-auto mt-auto`}>Read More</span>
+        </Card>
       </button>
 
       <ModalDialog ref={dialogRef}>
