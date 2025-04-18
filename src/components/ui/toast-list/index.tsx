0d72be98881775447b9838ff1251e5886f8d3156
Arjun Puri <arjun_261094@hotmail.com>
2025-04-18 17:05:26 +0200

    add padding to toast list on mobile

--- src/components/ui/toast-list/index.tsx
+++ src/components/ui/toast-list/index.tsx
@@ -8,7 +8,7 @@ export const ToastList = () => {
   const { toasts } = useToast();
 
   return (
-    <div className="fixed bottom-8 right-1/2 z-50 w-full translate-x-1/2 md:right-4 md:max-w-80 md:translate-x-0">
+    <div className="fixed bottom-8 right-1/2 z-50 w-full translate-x-1/2 px-6 md:right-4 md:max-w-80 md:translate-x-0 md:px-0">
       <ul className="flex flex-col gap-4">
         <AnimatePresence mode="popLayout">
           {toasts.map((toast) => (
