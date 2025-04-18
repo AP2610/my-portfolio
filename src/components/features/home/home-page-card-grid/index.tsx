a9baf6341e75ef6dd33f3e19ba9cd1de54707cf6
Arjun Puri <arjun_261094@hotmail.com>
2025-04-18 17:37:53 +0200

    uSe heading component and adjust layout for mobile

--- src/components/features/home/home-page-card-grid/index.tsx
+++ src/components/features/home/home-page-card-grid/index.tsx
@@ -4,22 +4,31 @@ import { SocialIconLink } from '@/components/ui/social-icon-link';
 import { FaArrowDown } from 'react-icons/fa';
 import { SpecialisationsIconList } from '../specialisations-icon-list';
 import { Card } from '@/components/ui/card';
+import { Heading } from '@/components/ui/heading';
 
 export const HomepageCardGrid = () => {
   return (
-    <div className="grid h-full auto-rows-auto grid-cols-1 gap-6 sm:grid-cols-2 lg:auto-rows-fr lg:grid-cols-12">
+    <div className="grid h-full auto-rows-auto grid-cols-2 gap-6 sm:grid-cols-2 lg:auto-rows-fr lg:grid-cols-12">
       <Card className="lg:col-span-3">
-        <h2 className="mb-4 text-3xl font-light">5 years</h2>
+        <Heading level="h2" weight="light" className="mb-4">
+          5 years
+        </Heading>
+
         <p className="text-card-body-text-color">Of frontend experience</p>
       </Card>
 
       <Card className="lg:col-span-3">
-        <h2 className="mb-4 text-3xl font-light">10+ Projects</h2>
+        <Heading level="h2" weight="light" className="mb-4">
+          10+ Projects
+        </Heading>
+
         <p className="text-card-body-text-color">Delivered Successfully</p>
       </Card>
 
-      <Card className="gap-4 sm:col-span-2 lg:col-span-6 lg:row-span-2" textAlign="left" alignItems="start">
-        <h2 className="text-3xl font-light">About me</h2>
+      <Card className="col-span-2 gap-4 lg:col-span-6 lg:row-span-2" textAlign="left" alignItems="start">
+        <Heading level="h2" weight="light">
+          About me
+        </Heading>
 
         <p className="flex-grow text-card-body-text-color">
           I'm a frontend engineer with 5 years of experience. I create beautiful, interactive and accessible applications that
@@ -32,9 +41,11 @@ export const HomepageCardGrid = () => {
         </Button>
       </Card>
 
-      <Card className="lg:col-span-6" textAlign="left" alignItems="start">
+      <Card className="col-span-2 lg:col-span-6" textAlign="left" alignItems="start">
         <div className="mb-4 flex flex-wrap items-center justify-between gap-4">
-          <h2 className="text-3xl font-light">Tech</h2>
+          <Heading level="h2" weight="light">
+            Tech
+          </Heading>
 
           <SpecialisationsIconList />
         </div>
@@ -43,19 +54,21 @@ export const HomepageCardGrid = () => {
       </Card>
 
       <Card className="lg:col-span-4 xl:col-span-3">
-        <MyLink type="internal" href="/cv" showIcon className="text-3xl font-light">
+        <MyLink type="internal" href="/cv" showIcon className="text-2xl font-light md:text-3xl">
           CV
         </MyLink>
       </Card>
 
       <Card className="lg:col-span-4 xl:col-span-3">
-        <MyLink type="internal" href="/projects" showIcon className="text-3xl font-light">
+        <MyLink type="internal" href="/projects" showIcon className="text-2xl font-light md:text-3xl">
           Projects
         </MyLink>
       </Card>
 
-      <Card className="gap-4 lg:col-span-4 xl:col-span-6" shouldWrap flexDirection="row">
-        <p className="text-2xl font-light">Get in touch</p>
+      <Card className="col-span-2 gap-4 lg:col-span-4 xl:col-span-6" shouldWrap flexDirection="row">
+        <Heading level="h2" weight="light">
+          Get in touch
+        </Heading>
 
         <ul className="flex gap-4">
           <li>
