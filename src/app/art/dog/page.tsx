aa53366ff0bcd8e204a1004b400ff5cf82b50f96
Arjun Puri <arjun_261094@hotmail.com>
2025-05-04 21:22:41 +0200

    small tweaks

--- src/app/art/dog/page.tsx
+++ src/app/art/dog/page.tsx
@@ -1,31 +1,34 @@
+import { Section } from '@/components/layout/section';
 import styles from './style.module.scss';
 
 const Dog = () => {
   return (
-    <div className={styles.canvas}>
-      <div className={styles.container}>
-        <div className={styles.containerInner}>
-          <div className={styles.dog}>
-            <div className={styles.head}>
-              <div className={styles.eyes}></div>
-              <div className={styles.cheeks}></div>
-              <div className={styles.noseMouth}></div>
-              <div className={styles.tongue}></div>
+    <Section isFullWidth paddingBottom="small">
+      <div className={`${styles.canvas} -mt-header-height pb-header-height pt-header-height md:mt-0`}>
+        <div className={styles.container}>
+          <div className={styles.containerInner}>
+            <div className={styles.dog}>
+              <div className={styles.head}>
+                <div className={styles.eyes}></div>
+                <div className={styles.cheeks}></div>
+                <div className={styles.noseMouth}></div>
+                <div className={styles.tongue}></div>
+              </div>
+
+              <div className={styles.body}>
+                <div className={styles.legLeft}></div>
+                <div className={styles.legRight}></div>
+              </div>
+
+              <div className={styles.tail}></div>
+
+              <div className={`${styles.hindLegLeft} ${styles.hindLeg}`}></div>
+              <div className={`${styles.hindLegRight} ${styles.hindLeg}`}></div>
             </div>
-
-            <div className={styles.body}>
-              <div className={styles.legLeft}></div>
-              <div className={styles.legRight}></div>
-            </div>
-
-            <div className={styles.tail}></div>
-
-            <div className={`${styles.hindLegLeft} ${styles.hindLeg}`}></div>
-            <div className={`${styles.hindLegRight} ${styles.hindLeg}`}></div>
           </div>
         </div>
       </div>
-    </div>
+    </Section>
   );
 };
 
