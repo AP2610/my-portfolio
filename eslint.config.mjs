7faff199ad871e9f5c13dd1a678ce769abb8917f
Arjun Puri <arjun_261094@hotmail.com>
2025-07-21 19:39:10 +0200

    Update CV and dev setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
+import typescript from '@typescript-eslint/parser';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,9 +11,18 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript', 'plugin:prettier/recommended'),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'plugin:prettier/recommended', 'plugin:@typescript-eslint/recommended'),
   {
+    languageOptions: {
+      parser: typescript,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
     rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+      // '@typescript-eslint/ban-types': 'error',
       'prettier/prettier': [
         'warn',
         {
